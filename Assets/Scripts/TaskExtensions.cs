48ed54cf0014db07d6c8c9792fafc97fffd9005c
Evgeniy A <abramov_eugine@mail.ru>
2022-04-24 01:03:29 +0300

    Add TaskExtensions

---
+++ Assets/Scripts/TaskExtensions.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+public static class TaskExtensions
+{
+	public static async Task WaitUntil(Func<bool> predicate, CancellationToken? cancellationToken = null)
+	{
+		bool IsCanceled() => cancellationToken is { IsCancellationRequested: true };
+
+		while (!IsCanceled() && !predicate())
+		{
+			await Task.Yield();
+		}
+	}
+
+	public static async Task WaitUntil(Func<bool> predicate, int millisecondsDelay, CancellationToken? cancellationToken = null)
+	{
+		bool IsCanceled() => cancellationToken is { IsCancellationRequested: true };
+
+		while (!IsCanceled() && !predicate())
+		{
+			await Task.Delay(millisecondsDelay);
+		}
+	}
+}
